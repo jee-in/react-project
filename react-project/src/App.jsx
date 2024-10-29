b9b1750577fbe870765812e4caa028980c9cabd6
jee-in <chris309804@gmail.com>
2024-10-29 18:06:07 +0000

    feat: implement parent-child component

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -3,6 +3,7 @@
  * JSX를 쓰면 자바스크립트 안에 html 태그 같은 마크업을 넣어 뷰 작업을 편하게 할 수 있다.
  * const test_el = <div><h1>안녕</h1></div> 
  * 위와 같은 태그는 정확히는 DOM 요소가 아니라 React DOM을 구성하는 React component이다.
+ * JSX: 함수로 만들어진 컴포넌트를 html 태그를 사용하듯이 코드를 작성하는 방식
 */
 
 /* <---- 컴포넌트 밖 ----> */
@@ -13,32 +14,28 @@ import reactLogo from './assets/react.svg'    // 이미지 파일
 import viteLogo from '/vite.svg'
 import './App.css'                            // CSS 파일
 
+// 자식 컴포넌트
+function Child() {
+  return <div>나는 자식입니다.</div>
+}
+
+// 부모 컴포넌트
+function Parent() {
+  return <Child />;
+}
+
+// 할아버지 컴포넌트
+function GrandParent() {
+  return <Parent />
+}
+
 function App() {
   /* <---- 컴포넌트 안 ----> */
   const [count, setCount] = useState(0)
 
-  const handleClick = () => {
-    alert('클릭!');
-  };
-
-  return (
-    /* <---- HTML/JSX 영역  ----> */
-    // 자바스크립트를 쓸 수 있는 영역
-    <div
-      style={{
-        height: '100vh',
-        display: ' flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      { /* 이곳에 퀴즈를 위한 html 코드를 작성해 주세요 */ }
-      <p>이것은 내가 만든 App component입니다.</p>
-      <button type="button" onClick={handleClick}>클릭!</button>
-    </div>
-  )
+  // 자식 컴포넌트를 App Component에서 마치 HTML 태그를 쓰듯이 넣기
+  return <GrandParent />;
 }
 
-// 컴포넌트 밖: 내가 만든 컴포넌트를 밖으로 내보내기
+// 컴포넌트 밖: 내가 만든 컴포넌트를 밖으로 내보내기 = 렌더링
 export default App
