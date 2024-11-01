733c6ef73c8e80df487eba0aa73eab7b4cbefc46
jee-in <chris309804@gmail.com>
2024-11-01 10:56:04 +0000

    feat: separate components to external files

--- react-styling/src/App.jsx
+++ react-styling/src/App.jsx
@@ -2,34 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/Button.jsx';
+import User from './components/User.jsx';
 
-// 컴포넌트 분리하기
-
-// 1. 버튼 컴포넌트 생성
-function CustomButton({ color, className, onClick, children }) {
-  return (
-    <button 
-      style={{ color: color }}
-      className={className} 
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-}
-
-// User 컴포넌트를 분리해서 구현
-function User({ user, handleDelete }) {
-  return (
-    <div className="squareStyle" id= { user.key }>
-      <div>{ user.name }</div>
-      <div>({ user.age })</div>
-      <CustomButton color="white" className="btn btn-del" onClick={() => handleDelete(user.id)}>
-      삭제하기
-      </CustomButton>
-    </div>
-  )
-}
+// Button, User 컴포넌트를 외부 파일로 분리함 - src > components > Button.js, User.js
 
 function App() {
 
@@ -42,7 +18,7 @@ function App() {
     { id: 5, age: 25, name: "오지니" },
     { id: 6, age: 25, name: "옹심이" },
   ]);
-
+  
   // 유저의 입력값을 담을 상태
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
@@ -53,19 +29,18 @@ function App() {
     // id를 직접 증가시키는 방식은 비동기 상태 업데이트에 문제가 있을 수 있다.
     // setId(id + 1)을 호출할 때 id의 최신 값이 아닐 수 있기 때문에,
     // 여러 번 호출될 경우 같은 id를 가지는 유저가 추가될 수 있다.
-    
-
+  
     // 콜백 함수
     setUsers((prevUsers) => {
       const newUser = {
-        id: prevUsers.length > 0 ? prevUsers[prevUsers.length - 1].id + 1 : 1,
+        id: id,
         age: age,
         name: name,
       };
-  
+
       return [...prevUsers, newUser];
     });
-    
+      
     setId((prevId) => prevId + 1);
 
     // input 태그 초기화
@@ -77,7 +52,7 @@ function App() {
   const deleteUserHandler = (id) => {
     const newUserList = users.filter((user) => user.id !== id);
     setUsers(newUserList);
-  }
+  }  
 
   return (
     <div className="app-container">
@@ -92,13 +67,13 @@ function App() {
         // input event로 들어온 입력값을 age의 값으로 업데이트
         onChange={(e) => setAge(e.target.value)}
         />
-        <CustomButton className={"btn btn-add"} onClick={addUserHandler} >추가하기</CustomButton>
+        <Button className={"btn btn-add"} onClick={addUserHandler} >추가하기</Button>
       </div>
       <div className="style">
         {users
           // .filter((user) => user.age <= 26 )
           .map((user) => {
-            if (user.age <= 26) {
+            if (user.age <= 24) {
               return null;
             }
 
