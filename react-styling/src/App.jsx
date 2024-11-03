c11182fd59062348d80b16c4dac59cfea03c9118
jee-in <chris309804@gmail.com>
2024-11-03 01:43:27 +0000

    feat: add change handler using useRef

--- react-styling/src/App.jsx
+++ react-styling/src/App.jsx
@@ -1,32 +1,33 @@
-import React, { useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 function App() {
-  // ref 값은 컴포넌트가 계속해서 렌더링되어도 unmount 전까지 값을 유지합니다.
-  // state: state는 변화가 일어나면 다시 렌더링이 일어난다.내부 변수들은 초기화된다.
-  // ref:   ref에 저장한 값은 렌더링을 일으키지 않는다. ref의 값 변화가 일어나도 렌더링으로 인해 내부 변수들이 초기화되는 것을 막을 수 있다. 컴포넌트가 여러 번 렌더링되어도 ref에 저장한 값은 유지된다.
+  const [id, setId] = useState("");
+  const idRef = useRef("");
+  const pwRef = useRef("");
 
-  const [count, setCount] = useState(0);
-  const countRef = useRef(0);
+  // 렌더링이 될 때
+  useEffect(() => {
+    idRef.current.focus();
+  }, []);
 
-  const plusStateCountButtonHandler = () => {
-    console.log('plusStateCountButtonHandler 호출');
-    setCount(count + 1);
+  // 아이디 10글자 이상 입력 시 비밀번호 인풋 태그로 포커스를 이동
+  const onIdChangeHandler = (event) => {
+    setId(event.target.value);
   };
 
-  const plusRefCountButtonHandler = () => {
-    console.log('plusRefCountButtonHandler 호출');
-    countRef.current++;
-  };
+  useEffect(() => {
+    if (id.length >= 10) {
+      pwRef.current.focus();
+    }
+  }, [id]);
 
   return (
     <>
       <div>
-        state 영역입니다. {count} <br />
-        <button onClick={plusStateCountButtonHandler}>state 증가</button>
+        아이디: <input type="text" ref={idRef} value={id} onChange={onIdChangeHandler}/>
       </div>
       <div>
-        ref 영역입니다. {countRef.current} <br />
-        <button onClick={plusRefCountButtonHandler}>ref 증가</button>
+        비밀번호: <input type="password" ref={pwRef} />
       </div>
     </>
   )
