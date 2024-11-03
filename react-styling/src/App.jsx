05d634f3301ba96c6939afe15e43bb582f04b452
jee-in <chris309804@gmail.com>
2024-11-03 16:51:45 +0000

    feat: send action object through useDispatch to reducer

--- react-styling/src/App.jsx
+++ react-styling/src/App.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
-  const counterStore = useSelector((state) => state); // 추가해주세요.
-  console.log(counterStore); // 스토어를 조회해볼까요?
+  const dispatch = useDispatch(); // dispatch 함수 생성
+  
+  const number = useSelector((state) => state.counter.number);
+  console.log(number);
 
-  return <div></div>;
-}
+  return (
+    <div>
+      {number}
+      <button
+        onClick={() => {
+          dispatch({ type: "PLUS_ONE" });
+        }}
+      >
+        + 1
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "MINUS_ONE" });
+        }}
+      >
+        - 1
+      </button>
+    </div>
+  );
+};
 
 export default App;
--- react-styling/src/redux/modules/counter.jsx
+++ react-styling/src/redux/modules/counter.jsx
@@ -5,7 +5,19 @@ const initialState = {
 
 // 리듀서
 const counter = (state = initialState, action) => {
+  // 리듀서가 전달받은 action을 콘솔에 출력해보기
+  console.log(action);
+
   switch (action.type) {
+    case "PLUS_ONE":
+      return {
+        // 기존 state에 있던 number에 +1을 더한다.
+        number: state.number + 1,
+      }
+    case "MINUS_ONE":
+      return {
+        number: state.number -1,
+      }
     default:
       return state;
   }
