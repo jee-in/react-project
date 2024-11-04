6e4e5c4b80c6c2a0ffa6b1f9ac77f8864ab2ef41
jee-in <chris309804@gmail.com>
2024-11-04 02:22:10 +0000

    feat: add useNavigate

--- react-styling/src/pages/Home.jsx
+++ react-styling/src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  return (
+    <button
+      onClick={() => {
+        navigate("/works");
+      }}
+    >
+      works로 이동
+    </button>
+  );
 };
 
 export default Home;
