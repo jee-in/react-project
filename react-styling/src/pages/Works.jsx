a97733598e7260e1419c696da10688734ade5767
jee-in <chris309804@gmail.com>
2024-11-04 03:32:00 +0000

    feat: set dynamic route

--- react-styling/src/pages/Works.jsx
+++ react-styling/src/pages/Works.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
 
 const Works = () => {
-  const location = useLocation();
-  console.log("location :>> ", location);
-
-  return (
-    <div>
-      <div>{`현재 페이지: ${location.pathname.slice(1)}`}</div>
-      <Link to="/contact">contact 페이지로 이동하기</Link>
-    </div>
-  );
+  return <div>Works</div>
 };
 
 export default Works;
