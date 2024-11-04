f590d5d080212d69f37041cf9b25ba8b9b8546e4
jee-in <chris309804@gmail.com>
2024-11-04 02:24:39 +0000

    feat: add useLocation

--- react-styling/src/pages/Works.jsx
+++ react-styling/src/pages/Works.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Works = () => {
-  return <div>Works</div>;
+  const location = useLocation();
+  console.log("location :>> ", location);
+
+  return (
+    <div>
+      <div>{`현재 페이지: ${location.pathname.slice(1)}`}</div>
+    </div>
+  );
 };
 
 export default Works;
